6f035b380a56517bc7a47262c8f31d82f52c84ad
CHITTAJALLU   LALITHA KALYAN <77020331+kalyan0309@users.noreply.github.com>
2023-03-08 13:47:53 +0530

    Create 2545. Sort the Students by Their Kth Scorel.py

---
+++ 2545. Sort the Students by Their Kth Scorel.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        d={}
+        for i in score:
+            d[i[k]]=i
+        keys = list(d.keys())
+        keys.sort(reverse=True)
+        return [d[i] for i in keys]
