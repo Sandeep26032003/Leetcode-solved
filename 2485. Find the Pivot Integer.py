7fa07c74987a3a0839287b061a3fa0d5309de35f
CHITTAJALLU   LALITHA KALYAN <77020331+kalyan0309@users.noreply.github.com>
2023-03-14 22:44:04 +0530

    Create 2485. Find the Pivot Integer.py

---
+++ 2485. Find the Pivot Integer.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        lst=[]
+        for i in range(1,n+1):
+            lst.append(i)
+        for i in range(len(lst)):
+            if sum(lst[:i])==sum(lst[i+1:]):
+                return lst[i]
+        return -1
