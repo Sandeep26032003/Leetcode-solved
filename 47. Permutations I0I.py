ad956544bdc9ac5b193eaee75afaef8a76042e34
CHITTAJALLU   LALITHA KALYAN <77020331+kalyan0309@users.noreply.github.com>
2023-06-17 23:26:55 +0530

    Create 47. Permutations I0I.py

---
+++ 47. Permutations I0I.py
@@ -0,0 +1,9 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        per = permutations(nums)
+        l = []
+        for i in per:
+            if i not in l:
+                l.append(i)
+        return l
+        
